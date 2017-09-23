7142d157f072b6ada91b336f3da21929a3f5db7e
YjWoo <yj_woo@126.com>
2017-09-23 22:53:15 +0800

    Daily Practice
    Signed-off-by: YjWoo <yj_woo@126.com>

---
+++ src/sword/to/offer2/Z_GetNextNode.java
@@ -0,0 +1,32 @@
+package sword.to.offer2;
+
+public class Z_GetNextNode {
+	public class TreeLinkNode {
+		int val;
+		TreeLinkNode left = null;
+		TreeLinkNode right = null;
+		TreeLinkNode next = null;
+
+		TreeLinkNode(int val) {
+			this.val = val;
+		}
+	}
+
+	// 找出中序遍历顺序的下一个结点并且返回
+	public TreeLinkNode GetNext(TreeLinkNode pNode) {
+		if (pNode == null)
+			return null;
+		if (pNode.right != null) { // 如果有右子树，则找右子树的最左节点
+			pNode = pNode.right;
+			while (pNode.left != null)
+				pNode = pNode.left;
+			return pNode;
+		}
+		while (pNode.next != null) { // 没右子树，则找第一个当前节点是父节点左孩子的节点
+			if (pNode.next.left == pNode)
+				return pNode.next;
+			pNode = pNode.next;
+		}
+		return null; // 退到了根节点仍没找到，则返回null
+	}
+}
