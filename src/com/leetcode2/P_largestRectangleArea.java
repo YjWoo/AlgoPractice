a491c398196cdd091c6e3b7806ad6a44edb2d64d
YjWoo <yj_woo@126.com>
2017-10-21 22:06:49 +0800

    Daily Practice
    Signed-off-by: YjWoo <yj_woo@126.com>

--- src/com/leetcode2/P_largestRectangleArea.java
+++ src/com/leetcode2/P_largestRectangleArea.java
@@ -1,6 +1,27 @@
 package com.leetcode2;
 
+import java.util.Stack;
+
 public class P_largestRectangleArea {
+	
+	// 给定矩阵画直方图，返回最大矩形面积。使用栈，时间复杂度低。
+	public int largestRectangleAreaByStack(int[] height) {
+        int len = height.length;
+        Stack<Integer> s = new Stack<Integer>();
+        int maxArea = 0;
+        for(int i = 0; i <= len; i++){
+            int h = (i == len ? 0 : height[i]);
+            if(s.isEmpty() || h >= height[s.peek()]){
+                s.push(i);
+            }else{
+                int tp = s.pop();
+                maxArea = Math.max(maxArea, height[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
+                i--;
+            }
+        }
+        return maxArea;
+    }
+	
 	// 给定矩阵画直方图，返回最大矩形面积
 	public int largestRectangleArea(int[] heights) {
 		if (heights.length == 0)
