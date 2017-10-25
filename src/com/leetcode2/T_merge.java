ff1cfc4e9ccfe21b8b11434e8ea864e4e1ac70e5
YjWoo <yj_woo@126.com>
2017-10-25 23:15:39 +0800

    Daily Practice
    Signed-off-by: YjWoo <yj_woo@126.com>

---
+++ src/com/leetcode2/T_merge.java
@@ -0,0 +1,18 @@
+package com.leetcode2;
+
+public class T_merge {
+	// 两个数组合并排序
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int i = m - 1;
+		int j = n - 1;
+		int k = m + n - 1;
+		while (i >= 0 && j >= 0) {
+			if (nums1[i] > nums2[j])
+				nums1[k--] = nums1[i--];
+			else
+				nums1[k--] = nums2[j--];
+		}
+		while (j >= 0)
+			nums1[k--] = nums2[j--];
+	}
+}
