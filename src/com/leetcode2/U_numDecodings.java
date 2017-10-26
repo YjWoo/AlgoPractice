bf670fa08b19f7b6c8ded02a735506822e1442ae
YjWoo <yj_woo@126.com>
2017-10-26 23:20:21 +0800

    Daily Practice
    Signed-off-by: YjWoo <yj_woo@126.com>

---
+++ src/com/leetcode2/U_numDecodings.java
@@ -0,0 +1,22 @@
+package com.leetcode2;
+
+public class U_numDecodings {
+	// 将字符串转为数字
+	public int numDecodings(String s) {
+		int n = s.length();
+		if (n == 0)
+			return 0;
+
+		int[] memo = new int[n + 1];
+		memo[n] = 1;
+		memo[n - 1] = s.charAt(n - 1) != '0' ? 1 : 0;
+
+		for (int i = n - 2; i >= 0; i--)
+			if (s.charAt(i) == '0')
+				continue;
+			else
+				memo[i] = (Integer.parseInt(s.substring(i, i + 2)) <= 26) ? memo[i + 1] + memo[i + 2] : memo[i + 1];
+
+		return memo[0];
+	}
+}
