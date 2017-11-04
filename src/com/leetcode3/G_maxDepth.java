8131802fcfd20c92243deff4023b3938c530b20d
YjWoo <yj_woo@126.com>
2017-11-04 22:51:30 +0800

    Daily Practice
    Signed-off-by: YjWoo <yj_woo@126.com>

---
+++ src/com/leetcode3/G_maxDepth.java
@@ -0,0 +1,21 @@
+package com.leetcode3;
+
+public class G_maxDepth {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	// 返回二叉树高度
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+	}
+}
