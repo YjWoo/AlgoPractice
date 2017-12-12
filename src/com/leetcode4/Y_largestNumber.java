ed277e1c2b828eb344d03603debf455747930c3a
YjWoo <yj_woo@126.com>
2017-12-12 19:05:07 +0800

    Daily Practice
    Signed-off-by: YjWoo <yj_woo@126.com>

---
+++ src/com/leetcode4/Y_largestNumber.java
@@ -0,0 +1,41 @@
+package com.leetcode4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Y_largestNumber {
+	// 将数组内所有值组合为一个最大的整数
+	public String largestNumber(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return "";
+
+		String[] s_num = new String[nums.length];
+		for (int i = 0; i < nums.length; i++)
+			s_num[i] = String.valueOf(nums[i]);
+
+		Comparator<String> comp = new Comparator<String>() {
+			@Override
+			public int compare(String str1, String str2) {
+				String s1 = str1 + str2;
+				String s2 = str2 + str1;
+				return s2.compareTo(s1); // reverse order here, so we can do
+											// append() later
+			}
+		};
+
+		Arrays.sort(s_num, comp);
+		if (s_num[0].charAt(0) == '0')
+			return "0";
+
+		StringBuilder sb = new StringBuilder();
+		for (String s : s_num)
+			sb.append(s);
+
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 67, 43, 213, 324, 6, 43, 123, 12, 4, 23 };
+		System.out.println(new Y_largestNumber().largestNumber(array));
+	}
+}
