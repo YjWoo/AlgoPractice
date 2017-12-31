0f3ca659bf6821c7aec1e611edda05e49dc6b7dc
YjWoo <yj_woo@126.com>
2017-12-31 22:43:43 +0800

    Daily Practice
    Signed-off-by: YjWoo <yj_woo@126.com>

---
+++ src/com/leetcode5/V_maximalSquare.java
@@ -0,0 +1,22 @@
+package com.leetcode5;
+
+public class V_maximalSquare {
+	// 返回二进制矩阵内'1'组成的最大方阵面积
+	public int maximalSquare(char[][] matrix) {
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+			return 0;
+
+		int max = 0, n = matrix.length, m = matrix[0].length;
+		int[][] dp = new int[n + 1][m + 1];
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (matrix[i - 1][j - 1] == '1') {
+					dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+					max = Math.max(max, dp[i][j]);
+				}
+			}
+		}
+		return max * max;
+	}
+}
