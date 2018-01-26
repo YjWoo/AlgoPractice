a370d0a919e53a3181a03dd4b486e05cc0b11610
YjWoo <yj_woo@126.com>
2018-01-26 22:55:43 +0800

    Daily Practice
    Signed-off-by: YjWoo <yj_woo@126.com>

--- src/com/leetcode6/W_getHint.java
+++ src/com/leetcode6/W_getHint.java
@@ -25,8 +25,29 @@ public class W_getHint {
 		return String.format("%dA%dB", numA, numB);
 	}
 
+	// 另一种方式，更低的时间、空间复杂度
+	public String getHint2(String secret, String guess) {
+		int numA = 0, numB = 0;
+		int[] numbers = new int[10];
+		for (int i = 0; i < secret.length(); i++) {
+			int s = Character.getNumericValue(secret.charAt(i));
+			int g = Character.getNumericValue(guess.charAt(i));
+			if (s == g)
+				numA++;
+			else {
+				if (numbers[s] < 0)
+					numB++;
+				if (numbers[g] > 0)
+					numB++;
+				numbers[s]++;
+				numbers[g]--;
+			}
+		}
+		return String.format("%dA%dB", numA, numB);
+	}
+
 	public static void main(String[] args) {
 		System.out.println(new W_getHint().getHint("347566", "548732"));
-		System.out.println(new W_getHint().getHint("748523", "548732"));
+		System.out.println(new W_getHint().getHint2("748523", "548732"));
 	}
 }
